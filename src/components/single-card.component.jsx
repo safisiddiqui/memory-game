33b77aa8f207aa4994d5e8140a09b12f791daf36
Safi Siddiqui <safisiddiquim@gmail.com>
2021-11-05 10:42:01 -0500

    finished project

---
+++ src/components/single-card.component.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+import './single-card.styles.css';
+
+
+const SingleCard = ({card, handleChoice, flipped, disabled}) => {
+
+
+const handleClick = () => {
+    if(!disabled) {
+
+        handleChoice(card)
+    }
+
+  }
+  
+
+
+
+    return (
+         <div className='card'>
+              <div className={flipped ? 'flipped' : ''}>
+                <img className='front'  
+                src={card.src} alt='card front' 
+                />
+                <img className='back'
+                 src ='/img/cover.png'  
+                onClick={handleClick} 
+                alt='card back' 
+                />
+              </div>
+            </div>
+            
+      );
+
+}
+
+export default SingleCard;
